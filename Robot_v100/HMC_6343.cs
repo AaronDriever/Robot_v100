76926192448025f9aee917cce39511f79eddcaea
AaronDriever <aarondriever@gmail.com>
2015-03-05 16:08:50 -0800

    lots and lots of changes

---
+++ Robot_v100/HMC_6343.cs
@@ -0,0 +1,124 @@
+using System;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+
+using System.Threading;
+namespace Robot_v100
+{
+    public class HMC_6343
+    {
+        const ushort HMC6343_ADDRESS = 0x32 >> 1;
+        const int CLOCK_FREQ = 100;
+        const int DELAY = 100;
+
+        //Write buffer
+        byte[] headingCommand = new byte[] { 0x50 }; // Read bearing command
+
+        //Read buffer
+        private byte[] inBuffer = new byte[6]; // Six bytes, MSB followed by LSB for each heading, pitch and roll
+
+        public float Heading
+        {
+            get;
+            private set;
+        }
+        public float Pitch
+        {
+            get;
+            private set;
+        }
+
+        public float Roll
+        {
+            get;
+            private set;
+        }
+
+        public float AccelX
+        {
+            get;
+            private set;
+        }
+        public float AccelY
+        {
+            get;
+            private set;
+        }
+        public float AccelZ
+        {
+            get;
+            private set;
+        }
+
+        //Create Read & Write transactions
+        readonly I2CDevice _deviceInterface;
+        readonly I2CDevice.I2CTransaction[] _txnPostHeading;
+        readonly I2CDevice.I2CTransaction[] _txnPostAccel;
+        readonly I2CDevice.I2CTransaction[] _txnReadData;
+
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HMC6343"/> class.
+        /// </summary>
+        public HMC_6343()
+        {
+       
+            _deviceInterface = new I2CDevice(new I2CDevice.Configuration((ushort)HMC6343_ADDRESS, CLOCK_FREQ));
+                 Thread.Sleep(500);
+            _txnPostHeading = new I2CDevice.I2CTransaction[] { I2CDevice.CreateWriteTransaction(headingCommand) };
+            _txnPostAccel = new I2CDevice.I2CTransaction[] { I2CDevice.CreateWriteTransaction(new byte[] { 0x40 }) };
+            _txnReadData = new I2CDevice.I2CTransaction[] { I2CDevice.CreateReadTransaction(inBuffer) };
+            byte[] opBuffer=new byte[1];
+
+            
+        }
+
+        /// <summary>
+        /// Releases unmanaged and - optionally - managed resources
+        /// </summary>
+        public void Dispose()
+        {
+            _deviceInterface.Dispose();
+        }
+        private void _decodeHeading(byte[] buffer)
+        {
+            int heading= (int)(((ushort)buffer[0]) << 8 | (ushort)buffer[1]);
+            int pitch = (int)(((ushort)buffer[2]) << 8 | (ushort)buffer[3]);
+            int roll = (int)(((ushort)buffer[4]) << 8 | (ushort)buffer[5]);
+            Heading = heading / 10.0f;
+            Pitch = (float)pitch / 10.0f;
+            if (Pitch > 6400)
+                Pitch = (Pitch - 6400 - 155);
+
+            Roll = (float)roll / 10.0f;
+            if (Roll > 6400)
+                Roll = (Roll - 6400 - 155);
+        }
+        private void _decodeAccel(byte[] buffer)
+        {
+            int x = (int)(((ushort)buffer[0]) << 8 | (ushort)buffer[1]);
+            int y = (int)(((ushort)buffer[2]) << 8 | (ushort)buffer[3]);
+            int z = (int)(((ushort)buffer[4]) << 8 | (ushort)buffer[5]);
+            AccelX = x;
+            AccelY = y;
+            AccelZ = z;
+        }
+        /// <summary>
+        /// Reads heading, pitch and roll and converts to usable degrees.
+        /// </summary>
+        public void Update()
+        {
+            _deviceInterface.Execute(_txnPostHeading, DELAY);
+            Thread.Sleep(1);  // Give the compass the requested 1ms delay before reading
+            int i=_deviceInterface.Execute(_txnReadData, DELAY);
+            _decodeHeading(inBuffer);
+
+            _deviceInterface.Execute(_txnPostAccel, DELAY);
+            Thread.Sleep(1);  // Give the compass the requested 1ms delay before reading
+            _deviceInterface.Execute(_txnReadData, DELAY);
+            _decodeAccel(inBuffer);
+
+        }
+
+    }
+}
--- Robot_v100/Program.cs
+++ Robot_v100/Program.cs
@@ -6,19 +6,260 @@ using GHIElectronics.NETMF.FEZ;
 using GHIElectronics.NETMF.Hardware;
 
 
+
 namespace Robot_v100
 {
     using HC_SR04; // this lets us use the HC_SR04.cs that is added.
+    public enum MoveDirection
+    {
+        Fwd, Back
+
+    }
+    public enum RotateDirection
+    {
+        CW,
+        CCW,
+    }
+    public sealed class Robot
+    {
+        private class Brain
+        {
+            Robot _parent;
+            SonarSensor _sonar;
+            HMC_6343 _compass;
+            public Brain(Robot parent,SonarSensor sensor, HMC_6343 compass)
+            {
+                _parent = parent;
+                _sonar = sensor;
+                _compass = compass;
+            }
+            public double FreeSpaceFwd
+            {
+                get;
+                private set;
+            }
+            public double FreeSpaceRight
+            {
+                get;
+                private set;
+            }
+            public double FreeSpaceLeft
+            {
+                get;
+                private set;
+            }
+            public double Heading
+            {
+                get
+                {
+                    return _compass.Heading;
+                }
+                
+            }
+            public double Pitch
+            {
+                get
+                {
+                    return _compass.Pitch;
+                }
+
+            }
+            public void UpdateCompass()
+            {
+                _compass.Update();
+                
+            }
+            public void UpdateRanges()
+            {
+                _parent._treads.Stop();
+
+            }
+        }
+        private class Servo
+        {
+            public static readonly uint OFF_VALUE = 1500;
+            private PWM _servoPin;
+            public Servo(FEZ_Pin.PWM pin)
+            {
+                _servoPin = new PWM((PWM.Pin)pin);
+                Value = OFF_VALUE;
+
+            }
+            private uint _pwmPeriod = 20000000;
+            private uint _pwmHigh;
+            public uint Value
+            {
+                get
+                {
+                    return _pwmHigh;
+                }
+                set
+                {
+                    _pwmHigh = value * 1000;
+                    _servoPin.SetPulse(_pwmPeriod, _pwmHigh);
+                }
+
+            }
 
 
+
+
+        }
+        private class SonarSensor
+        {
+            HC_SR04 _sensor;
+            public SonarSensor(FEZ_Pin.Digital trigger, FEZ_Pin.Digital echo)
+            {
+                _sensor = new HC_SR04((Cpu.Pin)trigger, (Cpu.Pin)echo);
+            }
+            public double CurrentDistance
+            {
+                get
+                {
+                    double Distance = -1;
+                    Debug.Print("Ping");
+                    long ticks = _sensor.Ping();
+                    if (ticks > 0L)
+                    {
+                        double centimeters = _sensor.TicksToInches(ticks) * 2.54;
+                        Debug.Print("Distance CM: " + centimeters.ToString());
+                        Distance = centimeters; //centimeters
+
+                    }
+                    return Distance;
+                }
+            }
+        }
+        private class Treads
+        {
+            private Robot _parent;
+            private Servo _treadR;
+            private Servo _treadL;
+            public Treads(Robot parent,FEZ_Pin.PWM left, FEZ_Pin.PWM right)
+            {
+                _treadL = new Servo(left);
+                _treadR = new Servo(right);
+            }
+            public void Rotate(RotateDirection dir, int duration)
+            {
+                switch (dir)
+                {
+                    case RotateDirection.CW:
+                        _treadL.Value = 1000;
+                        _treadR.Value = 1000;
+
+                        break;
+                    case RotateDirection.CCW:
+                        _treadL.Value = 2000;
+                        _treadR.Value = 2000;
+                        break;
+
+
+                }
+                Thread.Sleep(duration);
+                Stop();
+            }
+            
+            public void Rotate(double degrees){
+                _parent.MyBrain.UpdateCompass();
+                double newHeading=_parent.MyBrain.Heading+degrees;
+                if(newHeading>360){
+                }
+            }
+            public void RotateTo(RotateDirection dir, double toAngle)
+            {
+                //Figure out current difference between current heading and desired heading;
+                _parent.MyBrain.UpdateCompass();
+                double dA=_parent.MyBrain.Heading-toAngle;
+                
+                //While we're not within 5 degrees of our desired heading, we use the square of the values for speed, since we don't have an Abs(double) method and we don't want
+                //to use any sort of sqrt procedure.
+                while((dA*dA)>25){
+                     if(dA>0){
+                         Rotate(RotateDirection.CCW,100);
+                     }
+                     else{
+                         Rotate(RotateDirection.CW,100);
+                     }
+                    _parent.MyBrain.UpdateCompass();
+                }
+            }
+
+            /// <summary>
+            /// Drive straight either foward or backwards.
+            /// </summary>
+            /// <param name="dir">Which direction to drive</param>
+            /// <param name="duration">How far to drive</param>
+            public void Drive(MoveDirection dir, int duration)
+            {
+
+                switch (dir)
+                {
+                    case MoveDirection.Fwd:
+                        _treadL.Value = 1000;
+
+                        _treadR.Value = 2000;
+                        break;
+                    case MoveDirection.Back:
+                        _treadL.Value = 2000;
+
+                        _treadR.Value = 1000;
+                        break;
+                }
+                Thread.Sleep(duration);
+                Stop();
+            }
+            public void Drive(MoveDirection dir, double cm)
+            {
+            }
+            public void Stop()
+            {
+                _treadL.Value = Servo.OFF_VALUE;
+                _treadR.Value = Servo.OFF_VALUE;
+            }
+        }
+        private double _distanceR;
+        private double _distanceL;
+        private double _distanceF;
+        public Treads MyTreads;
+        public Brain MyBrain;
+        public Robot(
+            FEZ_Pin.PWM servoRPin,
+            FEZ_Pin.PWM servoLPin,
+            FEZ_Pin.Digital rangeTriggerPin,
+            FEZ_Pin.Digital rangeEchoPin)
+        {
+
+            MyTreads= new Treads(this,servoLPin, servoRPin);
+            MyBrain= new Brain(this,new SonarSensor(rangeTriggerPin, rangeEchoPin),new HMC_6343());
+
+        }
+
+
+
+
+        public void RobotLoop()
+        {
+            while (true)
+            {
+                Debug.Print("Current Distance: " + _rangeFinder.CurrentDistance.ToString());
+                Thread.Sleep(1000);
+                _treads.Rotate(RotateDirection.CW, 1000);
+                _treads.Rotate(RotateDirection.CCW, 1000);
+                //Set up new direction based on peripheral distances
+                //move foward until within 10 cm;
+
+            }
+        }
+    }
     public class Program
     {
         // put pin assignments here.
 
-        static PWM servoR = new PWM((PWM.Pin)FEZ_Pin.PWM.Di9); // Right servo control pin
-        static PWM servoL = new PWM((PWM.Pin)FEZ_Pin.PWM.Di8); // Left servo control pin
-        static HC_SR04 sensor = new HC_SR04((Cpu.Pin)FEZ_Pin.Digital.Di4, (Cpu.Pin)FEZ_Pin.Digital.Di5);
-        static int tock = 500;
+        //static PWM servoR = null; //new PWM((PWM.Pin)FEZ_Pin.PWM.Di9); // Right servo control pin
+        //static PWM servoL = null;// new PWM((PWM.Pin)FEZ_Pin.PWM.Di8); // Left servo control pin
+        //static HC_SR04 sensor = null; //new HC_SR04((Cpu.Pin)FEZ_Pin.Digital.Di4, (Cpu.Pin)FEZ_Pin.Digital.Di5);
+        //static int tock = 500;
 
         public static void Main()
         {
@@ -29,20 +270,15 @@ namespace Robot_v100
 
             //Start functions here.
 
+            Robot robot = new Robot(FEZ_Pin.PWM.Di9, FEZ_Pin.PWM.Di8, FEZ_Pin.Digital.Di4, FEZ_Pin.Digital.Di5);
+            robot.RobotLoop();
 
-
-            while (true)
-            {
-
-                AlphaProtocol();
-
-            }
         }
         /// <summary>
         /// 
         /// </summary>
         /// <returns></returns>
-        public static double GetDistance() // Ping!
+       /* public static double GetDistance() // Ping!
         {
             double Distance = 0;
 
@@ -113,6 +349,7 @@ namespace Robot_v100
 
         }
 
+
         class Move // move class
         {
             /// <summary>Movement Class.
@@ -156,7 +393,7 @@ namespace Robot_v100
                 servoR.SetPulse(20 * 1000 * 1000, 1500 * 1000); // right servo forward
             }
         }
-    }
+    }*/
 }
 /*
  The End!
